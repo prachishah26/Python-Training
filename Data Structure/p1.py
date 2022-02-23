4cc4ffcf3a42e03ad4c8f848368549eea3dd94ad
Prachi Shah <prachishah0567@gmail.com>
2022-02-23 19:34:08 +0530

    .

--- Data Structure/p1.py
+++ Data Structure/p1.py
@@ -1,16 +1,20 @@
 # Slice list into 3 equal chunks and reverse each chunk
 
-array1 =  [11, 45, 8, 23, 14, 12, 78, 45, 89]
-new_arr = []
-n = 3
-chunks = int((len(array1)/n))
-print(chunks)
-for i in range(chunks+1):
-    a = array1[0:3]
-    a.reverse()
-    new_arr.append(a)
-    array1 = array1[3:]
-    if len(array1)==0:
-        break
+raw_list =  [11, 45, 8, 23, 14, 12, 78, 45, 89, 1, 2]
+new_raw_list = []
+n = 5
+parts_of_rawdata = 2
+# chunks = int((len(raw_list)/n))
+for i in range(n):
+    if i != n-1:
+        a = raw_list[0:parts_of_rawdata]
+        a.reverse()
+        new_raw_list.append(a)
+        raw_list = raw_list[parts_of_rawdata:]
+    else:
+        a = raw_list[0:]
+        a.reverse()
+        new_raw_list.append(a)
+        
+print(new_raw_list)
 
-print(new_arr)
--- Data Structure/p2.py
+++ Data Structure/p2.py
@@ -2,10 +2,25 @@
 # Sample: [11, 45, 8, 11, 23, 45, 23, 45, 89]
 # Result: {11: 2, 45: 3, 8: 1, 23: 2, 89: 1}
 
-array = [11, 45, 8, 11, 23, 45, 23, 45, 89]
-myset = set(array)
-new_array = list(myset)
-dictionary = dict()
-for i in range(len(new_array)):
-    dictionary[new_array[i]] = array.count(new_array[i])
-print(dictionary)
+numbers = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+numbers_set = set(numbers)
+new_numbers = list(numbers_set)
+numbers_dict = dict()
+for i in range(len(new_numbers)):
+    numbers_dict[new_numbers[i]] = numbers.count(new_numbers[i])
+print(numbers_dict)
+
+
+# ===============================without count====================================
+
+numbers = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+numbers_set = set(numbers)
+numbers_dict = dict()
+for i in numbers:
+    if i not in numbers_dict.keys():
+        numbers_dict[i] = 1
+    else:
+        j = numbers_dict[i]
+        numbers_dict[i] = j+1
+
+print(numbers_dict)
