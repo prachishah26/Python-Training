aaf833339b3caecbb20f8c5c908da39b98810bf2
Prachi Shah <prachishah0567@gmail.com>
2022-02-24 16:57:07 +0530

    .

--- Data_Structure/program_08.py
+++ Data_Structure/program_08.py
@@ -1,11 +1,11 @@
 # Return the sum of duplicates elements from the given List
-# 			Ex. numbers = [3, 5, 6, 11, 12, 3, 5, 2, 3, 5,3]
+# 			Ex. numbers = [3, 5, 6, 11, 12, 3, 5, 2]
 # 			Output = 8 (3+5)
 
 numbers = [3, 5, 6, 11, 12, 3, 5, 2]
 repeated_value = set()
 for i in range(len(numbers)):
     if(numbers.count(numbers[i])>1):
-        repeated_value.add(((numbers.count(numbers[i]))-1)*numbers[i])
+        repeated_value.add(numbers[i])
 summation = sum(list(repeated_value))
 print(summation)
