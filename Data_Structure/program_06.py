3faa571db7b73224a469364b3058e975b75a233a
Prachi Shah <prachishah0567@gmail.com>
2022-02-24 16:35:22 +0530

    .

--- Data_Structure/program_06.py
+++ Data_Structure/program_06.py
@@ -1,17 +1,10 @@
 # Create a function to reverse the entire list without any function and also do not use any indexing or slicing shortcut. Time Complexity O(logn)
 
-
-numbers = [1,2,3,4,5,21]
+numbers = [1,2,3,4,5,21,65]
 n = len(numbers)
 def reverse_numbers(numbers):
     for i in range(len(numbers)):
-        if n%2 != 0:
-            if i < (n/2)-1:
-                c = numbers[i]
-                numbers[i] = numbers[n-i-1]
-                numbers[n-i-1] = c
-        else:
-            if i < n/2:
+            if i < (n/2):
                 c = numbers[i]
                 numbers[i] = numbers[n-i-1]
                 numbers[n-i-1] = c
