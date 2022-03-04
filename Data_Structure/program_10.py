d1eebd11710131035be4ee461570244e841e90bc
Prachi Shah <prachishah0567@gmail.com>
2022-03-04 16:47:00 +0530

    optimazation

--- Data_Structure/program_10.py
+++ Data_Structure/program_10.py
@@ -1,10 +1,10 @@
 # Find the max sum of sub array
 
 
-import math
+# import math
 numbers = [5, 4, 7, -2, 5, 0, 6, 9, 15, -3]
-
-total_max_sum = 0
+numbers = [-1,-2,-3,1,2]
+total_max_sum = -1000000
 temp_sum = 0
 
 for itr in range(0, len(numbers)):
@@ -12,17 +12,8 @@ for itr in range(0, len(numbers)):
     # print(temp_sum)
     if(temp_sum > total_max_sum):
         total_max_sum = temp_sum
-        if(temp_sum < 0):
-            temp_sum = 0       
+    if(temp_sum < 0):
+        temp_sum = 0       
 print(total_max_sum)
 
 
-# -------------------------------------------------------------------------------------
-
-# summation = []
-# for itr in range(len(numbers)):
-#     for j in range(len(numbers)):
-#         if itr!= j and itr<j:
-#             summation.append((sum(numbers[itr:j+1])))
-           
-# print(max(summation))
