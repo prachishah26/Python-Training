84af37099002f256a94aa4b71a0ccac4da620ea7
Prachi Shah <prachishah0567@gmail.com>
2022-03-04 15:58:46 +0530

    code optimization

--- Data_Structure/program_10.py
+++ Data_Structure/program_10.py
@@ -3,10 +3,26 @@
 
 import math
 numbers = [5, 4, 7, -2, 5, 0, 6, 9, 15, -3]
-summation = []
-for i in range(len(numbers)):
-    for j in range(len(numbers)):
-        if i!= j and i<j:
-            summation.append((sum(numbers[i:j+1])))
+
+total_max_sum = 0
+temp_sum = 0
+
+for itr in range(0, len(numbers)):
+    temp_sum += numbers[itr]
+    # print(temp_sum)
+    if(temp_sum > total_max_sum):
+        total_max_sum = temp_sum
+        if(temp_sum < 0):
+            temp_sum = 0       
+print(total_max_sum)
+
+
+# -------------------------------------------------------------------------------------
+
+# summation = []
+# for itr in range(len(numbers)):
+#     for j in range(len(numbers)):
+#         if itr!= j and itr<j:
+#             summation.append((sum(numbers[itr:j+1])))
            
-print(max(summation))
+# print(max(summation))
