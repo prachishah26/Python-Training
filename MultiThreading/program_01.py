85171975596b35e8ff54e31cfd491fe3f7655e29
Prachi Shah <prachishah0567@gmail.com>
2022-03-09 16:16:12 +0530

    commit

--- MultiThreading/program_01.py
+++ MultiThreading/program_01.py
@@ -3,28 +3,30 @@
 
 import threading, time
 result = 2
-def cube_of_numbers(result):
+def cube_of_numbers():
     """
     This functin will return cube of the particular number
     """
+    global result
     for i in range(3):
         time.sleep(1)
-        result *= result**3
+        result = result**3
         print("Cube: {}".format(result))
 
-def square_of_numbers(result):
+def square_of_numbers():
     """
     This function will return square of the particular number
     """
+    global result
     for i in range(3):
         time.sleep(1)
-        result *= result**2
+        result = result**2
         print("Square: {}".format(result))
 
 if __name__ == "__main__":
     # creating the thread
-    thread1 = threading.Thread(target=square_of_numbers, args=(result,))
-    thread2 = threading.Thread(target=cube_of_numbers, args=(result,))
+    thread1 = threading.Thread(target=square_of_numbers)
+    thread2 = threading.Thread(target=cube_of_numbers)
 
     # starting thread thread1
     thread1.start()
